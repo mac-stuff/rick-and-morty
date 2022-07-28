8be7f3a413aa684e6cd545eac1764ba6c3544d19
marika <ms.czerwonka@gmail.com>
2022-07-28 21:26:03 +0200

    added Person component

--- src/components/character-components/Card.js
+++ src/components/character-components/Card.js
@@ -1,20 +1,12 @@
 import "./Card.css";
+import Person from "./Person";
 
 const Card = ({ results }) => {
   let display = "No found!";
 
   if (results) {
     display = results.map((result) => {
-      return (
-        <div key={result.id} className="card">
-          <img src={result.image} alt="~" className="card-image" />
-          <div className="card-content">
-            <div className="card-text">{result.name}</div>
-            <div className="card-text">{result.species}</div>
-            <div className="card-text">{result.status}</div>
-          </div>
-        </div>
-      );
+      return <Person result={result} />;
     });
   }
 
